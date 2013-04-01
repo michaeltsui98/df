01066d64ca2e9559f17e9955e050e9f81c6e872f
Administrator <Administrator@PC-20121009BBUD>
2013-04-01 11:24:24 +0800

    33333

--- Cola/Exception.php
+++ Cola/Exception.php
@@ -2,46 +2,40 @@
 
 class Cola_Exception extends Exception
 {
-
-    /* public function __construct($message, $code = 0, $previous = NULL)
-    {
-        parent::__construct($message, $code, $previous);
-    } */
-
-    public function log()
+    
+ 
+    public function log ()
     {
         $loger = new Cola_Com_Log_File(Cola::$_config->get('_log'));
         $loger->error($this->getString());
     }
 
-    public function sendMail()
+    public function sendMail ()
     {
         $email = new Cola_Com_Email(Cola::$_config->get('_emailQueue'));
-        $email::put(Cola::$_config->get('_errorReportMail'), 'Exception message', $this->getString(), 'HTML');
+        $email::put(Cola::$_config->get('_errorReportMail'), 
+                'Exception message', $this->getString(), 'HTML');
     }
 
-    public function debug()
+    public function debug ()
     {
         if (defined('DEBUG') && FALSE == DEBUG) {
             header("Location: /404.html", true, 302);
-            exit;
+            exit();
         } else {
             echo $this->getString();
         }
     }
 
-    public function getString()
+    public function getString ()
     {
         $string = $this->getFile() . '(' . $this->getLine() . '): ';
-        $string .= $this->getMessage() . ($this->getCode() > 0 ? '(S#' . $this->getCode() . ')' : '');
+        $string .= $this->getMessage() .
+                 ($this->getCode() > 0 ? '(S#' . $this->getCode() . ')' : '');
         return $string;
     }
-
-    /* public function __toString()
-    {
-        return $this->getString();
-    } */
     
+ 
     public static $php_errors = array(
             E_ERROR => 'Fatal Error',
             E_USER_ERROR => 'User Error',
@@ -52,9 +46,9 @@ class Cola_Exception extends Exception
             E_NOTICE => 'Notice',
             E_RECOVERABLE_ERROR => 'Recoverable Error'
     );
-    
+
     public static $error_view = '';
-    
+
     public function __construct ($message, array $variables = NULL, $code = 0)
     {
         if (defined('E_DEPRECATED')) {
@@ -64,12 +58,12 @@ class Cola_Exception extends Exception
         $message = empty($variables) ? $message : strtr($message, $variables);
         parent::__construct($message, (int) $code);
     }
-    
+
     public function __toString ()
     {
         return self::text($this);
     }
-    
+
     public static function handler (Exception $e)
     {
         try {
@@ -86,24 +80,24 @@ class Cola_Exception extends Exception
                  * use XDebug to generate the stack trace
                  */
                 
-                if (function_exists('xdebug_get_function_stack') AND $code == E_ERROR)
-                {
-                    $trace = array_slice(array_reverse(xdebug_get_function_stack()), 4);
+                if (function_exists('xdebug_get_function_stack') and
+                         $code == E_ERROR) {
+                    $trace = array_slice(
+                            array_reverse(xdebug_get_function_stack()), 4);
                     
-                    foreach ($trace as & $frame)
-                    {
+                    foreach ($trace as & $frame) {
                         /**
-                         * XDebug pre 2.1.1 doesn't currently set the call type key
+                         * XDebug pre 2.1.1 doesn't currently set the call type
+                         * key
                          * http://bugs.xdebug.org/view.php?id=695
                          */
-                        if ( ! isset($frame['type']))
-                        {
+                        if (! isset($frame['type'])) {
                             $frame['type'] = '??';
                         }
-                
-                        // XDebug also has a different name for the parameters array
-                        if (isset($frame['params']) AND ! isset($frame['args']))
-                        {
+                        
+                        // XDebug also has a different name for the parameters
+                        // array
+                        if (isset($frame['params']) and ! isset($frame['args'])) {
                             $frame['args'] = $frame['params'];
                         }
                     }
@@ -121,20 +115,20 @@ class Cola_Exception extends Exception
                     }
                 }
             }
-    
+            
             $error = self::text($e);
-    
+            
             /*
              * if(is_object(Keke::$_log)){ Keke::$_log->add(log::ERROR, $error);
-            * $strace = self::text($e)."\n--\n" . $e->getTraceAsString();
-            * Keke::$_log->add(log::STRACE, $strace); Keke::$_log->write(); }
-            */
+             * $strace = self::text($e)."\n--\n" . $e->getTraceAsString();
+             * Keke::$_log->add(log::STRACE, $strace); Keke::$_log->write(); }
+             */
             $data['type'] = $type;
             $data['code'] = $code;
             $data['message'] = $message;
             $data['file'] = $file;
             $data['line'] = $line;
-    
+            
             $vars = array(
                     '_SESSION',
                     '_GET',
@@ -144,9 +138,9 @@ class Cola_Exception extends Exception
                     '_SERVER'
             );
             
-            $data['trace'] = array_reverse( Cola_Exception_Debug::trace($trace));
+            $data['trace'] = array_reverse(Cola_Exception_Debug::trace($trace));
             
-            require COLA_DIR .DIRECTORY_SEPARATOR. 'Exception/ShowError.php';
+            require COLA_DIR . DIRECTORY_SEPARATOR . 'Exception/ShowError.php';
             die();
         } catch (Exception $e) {
             ob_get_level() and ob_clean();
@@ -154,12 +148,11 @@ class Cola_Exception extends Exception
             exit(1);
         }
     }
-    
+
     public static function text (Exception $e)
     {
-        return sprintf('%s [ %s ]: %s ~ %s [ %d ]', get_class($e),
-                $e->getCode(), strip_tags($e->getMessage()), $e->getFile(),
+        return sprintf('%s [ %s ]: %s ~ %s [ %d ]', get_class($e), 
+                $e->getCode(), strip_tags($e->getMessage()), $e->getFile(), 
                 $e->getLine());
     }
-
 }
