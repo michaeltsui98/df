09cab9a8601ae04d6152b403fdc8f38a54adde4c
michael <michael@michael-PC>
2013-05-23 14:01:31 +0800

    3232323

--- views/tpl_c/views_admin_login.php
+++ views/tpl_c/views_admin_login.php
@@ -1,4 +1,4 @@
-<?php Cola_Tpl::checkrefresh('views/admin/login', '1368770343' );?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php Cola_Tpl::checkrefresh('views/admin/login', '1369102266' );?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
